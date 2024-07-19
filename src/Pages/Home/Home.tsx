fa0b6de1d4f7215b108d3fd5a38c2437e611a90c
Antônio M. Quadros Filho <antonio@4.events>
2024-07-19 07:30:07 -0300

    react 18 + react-dom 18 + typescript

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <p>
+          Edit <code>src/Pages/Home/Home.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
